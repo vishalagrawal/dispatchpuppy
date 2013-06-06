81c4c9d66c1c9c0cbef9dddd8f1c90e393143e28
Vishal Agrawal <vishalagrawal14@gmail.com>
2013-06-06 11:06:46 -0400

    add helper function to get access to assets folder

---
+++ application/helpers/utility_helper.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+function asset_url(){
+   return base_url().'assets/';
+}
+
+/* End of file utility_helper.php */
+/* Location: ./application/helpers/utility_helper.php */
