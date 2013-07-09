ce50dc7580f942d9b774b948ff6ffda839d2b386
Vishal Agrawal <vishalagrawal14@gmail.com>
2013-07-09 16:41:37 -0400

    change names of files to make them more intuitive. add directory for color-pallet

--- application/controllers/add_lat_lng.php
+++ application/controllers/add_lat_lng.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Get_location extends CI_Controller {
+class Add_lat_lng extends CI_Controller {
 
 	public function index()
 	{
